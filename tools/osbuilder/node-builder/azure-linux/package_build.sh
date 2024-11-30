7137b153cb04bd7495cd31cab0812cd2da6a51eb
Manuel Huber <mahuber@microsoft.com>
2024-11-30 10:55:34 +0000

    node-builder: Adjust mem default values
    - Assign new, different values for kata and confpods
    
    Signed-off-by: Manuel Huber <mahuber@microsoft.com>

--- tools/osbuilder/node-builder/azure-linux/package_build.sh
+++ tools/osbuilder/node-builder/azure-linux/package_build.sh
@@ -21,7 +21,7 @@ source "${common_file}"
 
 # these options ensure we produce the proper CLH config file
 runtime_make_flags="SKIP_GO_VERSION_CHECK=1 QEMUCMD= FCCMD= ACRNCMD= STRATOVIRTCMD= DEFAULT_HYPERVISOR=cloud-hypervisor
-	DEFMEMSZ=256 DEFSTATICSANDBOXWORKLOADMEM=1792 DEFVIRTIOFSDAEMON=${VIRTIOFSD_BINARY_LOCATION} PREFIX=${INSTALL_PATH_PREFIX}"
+	DEFVIRTIOFSDAEMON=${VIRTIOFSD_BINARY_LOCATION} PREFIX=${INSTALL_PATH_PREFIX}"
 
 # - for vanilla Kata we use the kernel binary. For ConfPods we use IGVM, so no need to provide kernel path.
 # - for vanilla Kata we explicitly set DEFSTATICRESOURCEMGMT_CLH. For ConfPods,
@@ -29,9 +29,9 @@ runtime_make_flags="SKIP_GO_VERSION_CHECK=1 QEMUCMD= FCCMD= ACRNCMD= STRATOVIRTC
 # - for ConfPods we explicitly set the cloud-hypervisor path. The path is independent of the PREFIX variable
 #   as we have a single CLH binary for both vanilla Kata and ConfPods
 if [ "${CONF_PODS}" == "no" ]; then
-	runtime_make_flags+=" DEFSTATICRESOURCEMGMT_CLH=true KERNELPATH_CLH=${KERNEL_BINARY_LOCATION}"
+	runtime_make_flags+=" DEFMEMSZ=64 DEFSTATICRESOURCEMGMT_CLH=true KERNELPATH_CLH=${KERNEL_BINARY_LOCATION}"
 else
-	runtime_make_flags+=" CLHPATH=${CLOUD_HYPERVISOR_LOCATION}"
+	runtime_make_flags+=" DEFMEMSZ=160 CLHPATH=${CLOUD_HYPERVISOR_LOCATION}"
 fi
 
 # On Mariner 3.0 we use cgroupsv2 with a single sandbox cgroup
