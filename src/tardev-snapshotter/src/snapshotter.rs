766db224d21e9f16b5d8ecb69f747ac560d628b6
Mitch Zhu <mitchzhu@microsoft.com>
2024-12-14 00:54:49 +0000

    use rootfs

--- src/tardev-snapshotter/src/snapshotter.rs
+++ src/tardev-snapshotter/src/snapshotter.rs
@@ -308,7 +308,7 @@ impl Store {
             let overlay_mount = api::types::Mount {
                 r#type: "bind".into(),
                 source: overlay_target.to_string_lossy().into(),
-                target: String::new(),
+                target: "/rootfs".into(),
                 options: vec!["rbind".into(), "rw".into()],
             };
 
